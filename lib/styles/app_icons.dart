884d6928a82225eb6096f380957e123a3e63a8e7
Ahmed Tarek <ziad_ahmed199690@yahoo.com>
2024-09-13 17:07:08 +0300

    feat: Add flutter_svg & required assets and texts

---
+++ lib/styles/app_icons.dart
@@ -0,0 +1,13 @@
+abstract class AppIcons {
+  static const String _path = 'assets/icons/';
+
+  static const String changePassword = '${_path}change-password.svg';
+  static const String export = '${_path}export.svg';
+  static const String autoLock = '${_path}auto-lock.svg';
+  static const String lock = '${_path}lock.svg';
+  static const String faceId = '${_path}face-id.svg';
+  static const String walletConnect = '${_path}wallet-connect.svg';
+  static const String community = '${_path}community.svg';
+  static const String privacyPolicy = '${_path}privacy-policy.svg';
+  static const String externalLink = '${_path}external-link.svg';
+}
