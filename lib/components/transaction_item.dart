9cf43b874972044270080f9a86bd326bac0bb6a6
Ahmed Tarek <58788687+ahmed-tarek-salem@users.noreply.github.com>
2024-12-01 21:46:23 +0200

    Feature/restrict resend transaction option to qubic transfers (#217)
    * feat: Restrict resend option to only not pending qubic transfers

--- lib/components/transaction_item.dart
+++ lib/components/transaction_item.dart
@@ -1,3 +1,5 @@
+// ignore: depend_on_referenced_packages
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
@@ -5,30 +7,23 @@ import 'package:qubic_wallet/components/copy_button.dart';
 import 'package:qubic_wallet/components/mid_text_with_ellipsis.dart';
 import 'package:qubic_wallet/components/qubic_amount.dart';
 import 'package:qubic_wallet/components/transaction_details.dart';
-import 'package:qubic_wallet/components/transaction_resend.dart';
 import 'package:qubic_wallet/components/transaction_status_item.dart';
 import 'package:qubic_wallet/di.dart';
+import 'package:qubic_wallet/extensions/asThousands.dart';
 import 'package:qubic_wallet/flutter_flow/theme_paddings.dart';
 import 'package:qubic_wallet/helpers/copy_to_clipboard.dart';
-import 'package:qubic_wallet/helpers/re_auth_dialog.dart';
-import 'package:qubic_wallet/helpers/sendTransaction.dart';
-import 'package:qubic_wallet/helpers/global_snack_bar.dart';
+import 'package:qubic_wallet/l10n/l10n.dart';
 import 'package:qubic_wallet/models/qubic_list_vm.dart';
 import 'package:qubic_wallet/models/transaction_vm.dart';
 import 'package:qubic_wallet/pages/main/wallet_contents/explorer/explorer_result_page.dart';
-import 'package:qubic_wallet/resources/apis/archive/qubic_archive_api.dart';
-import 'package:qubic_wallet/resources/apis/live/qubic_live_api.dart';
-import 'package:qubic_wallet/resources/qubic_li.dart';
+import 'package:qubic_wallet/pages/main/wallet_contents/send.dart';
+import 'package:qubic_wallet/smart_contracts/qutil_info.dart';
+import 'package:qubic_wallet/smart_contracts/qx_info.dart';
 import 'package:qubic_wallet/stores/application_store.dart';
-// ignore: depend_on_referenced_packages
-import 'package:collection/collection.dart';
 import 'package:qubic_wallet/styles/text_styles.dart';
 import 'package:qubic_wallet/styles/themed_controls.dart';
-import 'package:qubic_wallet/timed_controller.dart';
+
 import 'transaction_direction_item.dart';
-import 'package:qubic_wallet/extensions/asThousands.dart';
-import 'package:qubic_wallet/l10n/l10n.dart';
-import 'package:qubic_wallet/helpers/target_tick.dart';
 
 enum CardItem { details, resend, explorer, clipboardCopy, delete }
 
@@ -36,61 +31,7 @@ class TransactionItem extends StatelessWidget {
   final TransactionVm item;
 
   TransactionItem({super.key, required this.item});
-  final _timedController = getIt<TimedController>();
-  final _globalSnackBar = getIt<GlobalSnackBar>();
-  final QubicLi _apiService = getIt<QubicLi>();
-  final _liveApi = getIt<QubicLiveApi>();
   final ApplicationStore appStore = getIt<ApplicationStore>();
-
-  Future<void> showResendDialog(BuildContext context) async {
-    final l10n = l10nOf(context);
-
-    return showDialog<void>(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: Text(l10n.sendItemDialogResendTitle,
-              style: TextStyles.alertHeader),
-          content: SingleChildScrollView(
-            child: TransactionResend(item: item),
-          ),
-          actions: <Widget>[
-            ThemedControls.transparentButtonBig(
-              text: l10n.generalButtonCancel,
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            ThemedControls.primaryButtonBig(
-              text: l10n.accountButtonSend,
-              onPressed: () async {
-                var result = await reAuthDialog(context);
-                if (!result) {
-                  return;
-                }
-
-                // get fresh latet tick
-                int latestTick = (await _liveApi.getCurrentTick()).tick;
-                int targetTick = latestTick + defaultTargetTickType.value;
-
-                bool success = await sendTransactionDialog(context,
-                    item.sourceId, item.destId, item.amount, targetTick);
-
-                if (success) {
-                  _globalSnackBar.show(
-                      l10n.generalSnackBarMessageTransactionSubmitted(
-                          targetTick!.asThousands()));
-                }
-                await _timedController.interruptFetchTimer();
-
-                Navigator.of(context).pop();
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
 
   //Gets the dropdown menu
   Widget getCardMenu(BuildContext context) {
@@ -101,11 +42,7 @@ class TransactionItem extends StatelessWidget {
             color: LightThemeColors.primary.withAlpha(140)),
         // Callback that sets the selected popup menu item.
         onSelected: (CardItem menuItem) async {
-          // setState(() {
-          //   selectedMenu = item;
-          // });
           if (menuItem == CardItem.explorer) {
-            //showRenameDialog(context);
             pushScreen(
               context,
               screen: ExplorerResultPage(
@@ -113,7 +50,7 @@ class TransactionItem extends StatelessWidget {
                 tick: item.targetTick,
                 focusedTransactionHash: item.id,
               ),
-              withNavBar: false, // OPTIONAL VALUE. True by default.
+              withNavBar: false,
               pageTransitionAnimation: PageTransitionAnimation.cupertino,
             );
           }
@@ -123,7 +60,16 @@ class TransactionItem extends StatelessWidget {
           }
 
           if (menuItem == CardItem.resend) {
-            showResendDialog(context);
+            pushScreen(
+              context,
+              screen: Send(
+                  amount: item.amount,
+                  destId: item.destId,
+                  item: appStore.currentQubicIDs
+                      .firstWhere((id) => id.publicId == item.sourceId)),
+              withNavBar: false,
+              pageTransitionAnimation: PageTransitionAnimation.cupertino,
+            );
           }
 
           if (menuItem == CardItem.details) {
@@ -148,14 +94,17 @@ class TransactionItem extends StatelessWidget {
                 value: CardItem.clipboardCopy,
                 child: Text(l10n.transactionItemButtonCopyToClipboard),
               ),
-              // sally: commenting this temporary until we improve the resend to support all transfers
-              //if (appStore.currentQubicIDs
-              //      .any((e) => e.publicId == item.sourceId) &&
-              //item.getStatus() != ComputedTransactionStatus.pending)
-              //PopupMenuItem<CardItem>(
-              //value: CardItem.resend,
-              //child: Text(l10n.transactionItemButtonResend),
-              //),
+              if (appStore.currentQubicIDs.any(
+                      (e) => e.publicId == item.sourceId && !e.watchOnly) &&
+                  item.getStatus() != ComputedTransactionStatus.pending &&
+                  item.amount > 0 &&
+                  item.destId != QxInfo.mainAssetIssuer &&
+                  item.destId != QutilInfo.address &&
+                  item.destId != QxInfo.address)
+                PopupMenuItem<CardItem>(
+                  value: CardItem.resend,
+                  child: Text(l10n.transactionItemButtonResend),
+                ),
               if (item.getStatus() == ComputedTransactionStatus.invalid)
                 PopupMenuItem<CardItem>(
                   value: CardItem.delete,
@@ -230,7 +179,6 @@ class TransactionItem extends StatelessWidget {
                           "transactionStatus${item.id}${item.getStatus().toString()}"),
                     ),
                   ),
-                  //                      TransactionStatusItem(item: item)
                   getCardMenu(context)
                 ])),
             Center(
