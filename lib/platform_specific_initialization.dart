6bc235ada3a0a9c9734fa9dd0a658cc686ad210a
sallymoc <sallymoc.qubic@gmail.com>
2024-12-05 18:51:21 +0100

    Enabling debugging in the web view only when not in release mode

--- lib/platform_specific_initialization.dart
+++ lib/platform_specific_initialization.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -20,7 +21,10 @@ class PlatformSpecificInitilization {
       if (await file.exists()) {
         await file.writeAsString('Fake');
       }
-      await InAppWebViewController.setWebContentsDebuggingEnabled(true);
+
+      if (!kReleaseMode) {
+        await InAppWebViewController.setWebContentsDebuggingEnabled(true);
+      }
     } catch (e) {
       debugPrint('Error: $e');
     }
