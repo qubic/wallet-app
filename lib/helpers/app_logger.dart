53571bb63b41780c2d974c0811c14e278a720a3c
Ahmed Tarek <ahmedtsalem9@gmail.com>
2024-12-13 14:31:21 +0200

    docs: Add logger docs

--- lib/helpers/app_logger.dart
+++ lib/helpers/app_logger.dart
@@ -1,5 +1,22 @@
 import 'package:logger/logger.dart';
 
+/// A logger instance that uses a custom printer to format log messages with colors
+/// and emojis, only in debug mode.
+///
+///  The logger uses the following log levels with corresponding emojis:
+/// - debug (🐛): Information useful for developers during development.
+/// - info (ℹ️): General informational messages.
+/// - warning (⚠️):  Potentially harmful situations which still allow the application to continue running.
+/// - error (❌): Error events that might still allow the application to continue running.
+/// - fatal (🔥):  Severe error events that when logged indicate a serious problem.
+///
+/// Example usage:
+/// ```dart
+/// appLogger.d('This is a debug message.');
+/// appLogger.i('This is an info message.');
+/// appLogger.w('This is a warning message.');
+/// appLogger.e('This is an error message.');
+///
 final appLogger = Logger(
   printer: CustomPrinter(),
   filter: null,
