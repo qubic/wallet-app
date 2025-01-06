4d3293a418e4039aa39c80ba73fac078c72de266
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-01-06 09:16:13 +0200

    feat: Complete qubic smart contracts info

--- lib/smart_contracts/sc_info.dart
+++ lib/smart_contracts/sc_info.dart
@@ -1,10 +1,35 @@
 enum QubicSCID {
-  qX(1, 'QX'),
-  quottery(2, 'Quottery'),
-  qRnd(3, 'Random');
+  qX(1, 'QX', 'BAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAARMID'),
+  quottery(2, 'Quottery',
+      'CAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACNKL'),
+  qRnd(3, 'Random',
+      'DAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAANMIG'),
+  qutil(4, 'QUtil',
+      'EAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAVWRF'),
+  mlm(5, 'My Last Match',
+      'FAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAYWJB'),
+  gqmProp(6, 'General Quorum Proposal',
+      'GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAQGNM'),
+  swatch(7, 'Supply Watcher',
+      'HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAHYCM'),
+  ccf(8, 'Computer Controller Fund',
+      'IAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABXSH'),
+  qEarn(9, 'QEarn',
+      'JAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAVKHO'),
+  qVault(10, 'QVault',
+      'KAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAXIUO');
 
-  const QubicSCID(this.contractIndex, this.name);
+  const QubicSCID(this.contractIndex, this.name, this.contractId);
 
   final int contractIndex;
   final String name;
+  final String contractId;
+
+  // Static map for quick lookup by contract ID
+  static final Map<String, QubicSCID> _byId = {
+    for (var scid in QubicSCID.values) scid.contractId: scid,
+  };
+
+  /// Get SC name by contract ID
+  static String? fromContractId(String id) => _byId[id]?.name;
 }
