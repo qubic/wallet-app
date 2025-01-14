a73122bc5e22ac0cccef24b834b39b59a7bc5c8b
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-01-14 23:59:27 +0200

    fix: Round usd balance to two decimal digits

--- lib/helpers/currency_helpers.dart
+++ lib/helpers/currency_helpers.dart
@@ -2,8 +2,6 @@ import 'package:intl/intl.dart';
 
 class CurrencyHelpers {
   static String formatToUsdCurrency(num amount) {
-    int decimalDigits = amount < 1 ? 10 : 2;
-    return NumberFormat.currency(symbol: '\$', decimalDigits: decimalDigits)
-        .format(amount);
+    return NumberFormat.currency(symbol: '\$', decimalDigits: 2).format(amount);
   }
 }
