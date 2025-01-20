91b4242af241419ac476f2bc9a59ad4d17c4adc2
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-01-20 15:08:24 +0200

    fix: Running app on macos error

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,17 +1,23 @@
 import Cocoa
 import FlutterMacOS
+import app_links
 
 @main
 class AppDelegate: FlutterAppDelegate {
-  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplicationcontinue userActivity: NSUserActivity,
-                                 restorationHandler: @escaping ([any NSUserActivityRestoring]) -> Void) -> Bool {
-
-  guard let url = AppLinks.shared.getUniversalLink(userActivity) else {
-    return false
+  
+  override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+    return true
   }
   
-  AppLinks.shared.handleLink(link: url.absoluteString)
-  
-  return false // Returning true will stop the propagation to other packages
+  override func application(_ application: NSApplication,
+                            continue userActivity: NSUserActivity,
+                            restorationHandler: @escaping ([NSUserActivityRestoring]) -> Void) -> Bool {
+    guard let url = AppLinks.shared.getUniversalLink(userActivity) else {
+      return false
+    }
+    
+    AppLinks.shared.handleLink(link: url.absoluteString)
+    
+    return false // Returning true will stop propagation to other handlers
   }
 }
