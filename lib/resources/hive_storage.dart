5877b74717f809faffe94755c5a9d2ff31781815
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-02-06 00:53:03 +0200

    feat: Encrypt stored transactions with hive

--- lib/resources/hive_storage.dart
+++ lib/resources/hive_storage.dart
@@ -1,6 +1,9 @@
+import 'dart:convert';
+
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:qubic_wallet/helpers/app_logger.dart';
+import 'package:qubic_wallet/di.dart';
 import 'package:qubic_wallet/models/transaction_vm.dart';
+import 'package:qubic_wallet/resources/secure_storage.dart';
 
 enum HiveBoxesNames {
   storedTransactions,
@@ -10,17 +13,38 @@ class HiveStorage {
   HiveStorage() {
     _init();
   }
+
+  final _secureStorage = getIt<SecureStorage>();
   late final Box<TransactionVm> storedTransactions;
+  late final HiveAesCipher _encryptionCipher;
 
   Future<void> _init() async {
     await Hive.initFlutter();
     Hive.registerAdapter(TransactionVmAdapter());
-    openTransactionsBox();
+    await _loadEncryptionKey();
+    await openTransactionsBox();
+  }
+
+  /// Loads or generates a new encryption key and stores it securely.
+  Future<void> _loadEncryptionKey() async {
+    String? keyString = await _secureStorage.getHiveEncryptionKey();
+
+    if (keyString == null) {
+      final key = Hive.generateSecureKey();
+      final encodedKey = base64UrlEncode(key);
+      await _secureStorage.storeHiveEncryptionKey(encodedKey);
+      _encryptionCipher = HiveAesCipher(key);
+    } else {
+      final key = base64Decode(keyString);
+      _encryptionCipher = HiveAesCipher(key);
+    }
   }
 
   Future<void> openTransactionsBox() async {
     storedTransactions = await Hive.openBox<TransactionVm>(
-        HiveBoxesNames.storedTransactions.name);
+      HiveBoxesNames.storedTransactions.name,
+      encryptionCipher: _encryptionCipher,
+    );
   }
 
   void addStoredTransaction(TransactionVm transactionVm) {
@@ -37,5 +61,6 @@ class HiveStorage {
 
   Future<void> clear() async {
     await storedTransactions.clear();
+    _secureStorage.deleteHiveEncryptionKey();
   }
 }
