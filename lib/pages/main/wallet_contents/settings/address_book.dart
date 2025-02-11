01fced5b4cb121b38dfd81a7f898a9f68b0026a2
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-02-11 10:33:21 +0200

    feat: Add AddressBook screen ui

--- lib/pages/main/wallet_contents/settings/address_book.dart
+++ lib/pages/main/wallet_contents/settings/address_book.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:qubic_wallet/flutter_flow/theme_paddings.dart';
 import 'package:qubic_wallet/l10n/l10n.dart';
+import 'package:qubic_wallet/styles/edge_insets.dart';
+import 'package:qubic_wallet/styles/text_styles.dart';
+import 'package:qubic_wallet/styles/themed_controls.dart';
 
 class AddressBook extends StatelessWidget {
   const AddressBook({super.key});
@@ -13,6 +17,38 @@ class AddressBook extends StatelessWidget {
         title: Text(
           l10n.settingsLabelAddressBook,
         ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add, color: LightThemeColors.primary),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: ListView.separated(
+        padding: ThemeEdgeInsets.pageInsets.copyWith(
+            bottom: MediaQuery.of(context).padding.bottom +
+                ThemePaddings.normalPadding),
+        separatorBuilder: (context, index) =>
+            ThemedControls.spacerHorizontalNormal(),
+        itemCount: 10,
+        itemBuilder: (context, index) {
+          return ThemedControls.card(
+              child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Account 1",
+                style: TextStyles.accountName,
+              ),
+              ThemedControls.spacerVerticalMini(),
+              Text(
+                "RBMXEFMDFABRTBJIYIBOQZMAWKWCPMJIQVEQDKONOFPEFWLMXQECDGEBIRBM",
+                style: TextStyles.accountPublicId,
+              )
+            ],
+          ));
+        },
       ),
     );
   }
