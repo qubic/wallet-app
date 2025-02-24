4d442a8bbd5e61289f84eac64c75a7a5442fc7b8
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-02-24 02:14:39 +0200

    feat: Add featured app section

--- lib/models/dapp_model.dart
+++ lib/models/dapp_model.dart
@@ -12,7 +12,23 @@ class DAppModel {
   });
 }
 
+//TODO Adjust as needed
+final featureApp = DAppModel(
+  name: "Qubic",
+  icon: "assets/images/qx.jpg",
+  url: "https://qx.qubic.org",
+  description:
+      "QX is a decentralized exchange running as a smart contract on the Qubic network",
+);
+
+//TODO Adjust as needed
 final List<DAppModel> dAppsList = [
+  // DAppModel(
+  //   name: "Qubic",
+  //   icon: "https://www.google.com/s2/favicons?domain=qubic.com",
+  //   url: "https://qx.qubic.org",
+  //   description: "Qubic network",
+  // ),
   DAppModel(
     name: "Uniswap",
     icon:
@@ -21,10 +37,16 @@ final List<DAppModel> dAppsList = [
     description: "Ethereum blockchain-based",
   ),
   DAppModel(
-    name: "Qubic",
-    icon: "https://www.google.com/s2/favicons?domain=qubic.com",
-    url: "https://qubic.com",
-    description:
-        "Qubic is a decentralized social media platform that allows users to create their own decentralized identity and share content with the world.",
+    name: "OpenSea",
+    icon:
+        "https://pageflows.imgix.net/media/logos/opensea.png?auto=compress&ixlib=python-1.1.2&s=459d52d3fe14f0c5ab2ade3916d4c862",
+    url: "https://opensea.io",
+    description: "NFT marketplace",
+  ),
+  DAppModel(
+    name: "1inch",
+    icon: "https://1inch.io/img/pressRoom/1inch_without_text.webp",
+    url: "https://1inch.io",
+    description: "DEX aggregator",
   ),
 ];
--- lib/pages/main/tab_dapps/tab_dapps.dart
+++ lib/pages/main/tab_dapps/tab_dapps.dart
@@ -1,11 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:qubic_wallet/flutter_flow/theme_paddings.dart';
 import 'package:qubic_wallet/models/dapp_model.dart';
+import 'package:qubic_wallet/pages/main/tab_dapps/components/dapp_tile.dart';
+import 'package:qubic_wallet/pages/main/tab_dapps/webview_screen.dart';
 import 'package:qubic_wallet/styles/edge_insets.dart';
 import 'package:qubic_wallet/styles/text_styles.dart';
 import 'package:qubic_wallet/styles/themed_controls.dart';
 
-class TabDApps extends StatelessWidget {
+class TabDApps extends StatefulWidget {
   const TabDApps({super.key});
+
+  @override
+  State<TabDApps> createState() => _TabDAppsState();
+}
+
+class _TabDAppsState extends State<TabDApps> {
+  bool _isImagesLoaded = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _preloadImages();
+  }
+
+  Future<void> _preloadImages() async {
+    for (var dApp in dAppsList) {
+      await precacheImage(NetworkImage(dApp.icon), context);
+    }
+    if (mounted) {
+      await precacheImage(AssetImage(featureApp.icon), context);
+    }
+    setState(() {
+      _isImagesLoaded = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,46 +41,78 @@ class TabDApps extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
       ),
-      body: ListView(
-        padding: ThemeEdgeInsets.pageInsets,
-        children: [
-          Text(
-            "Popular dApps",
-            style: TextStyles.pageTitle,
-          ),
-          ThemedControls.card(
-              child: Column(
-            children: [
-              Row(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network(dAppsList[0].icon, height: 40),
-                  ),
-                  const SizedBox(width: 8),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          dAppsList[0].name,
-                          style: TextStyles.labelText,
+      body: _isImagesLoaded
+          ? ListView(
+              padding: ThemeEdgeInsets.pageInsets,
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) =>
+                                WebviewScreen(initialUrl: featureApp.url)));
+                  },
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      ShaderMask(
+                        shaderCallback: (Rect bounds) {
+                          return LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.black.withValues(alpha: 0.7),
+                              Colors.transparent, // Opacity 0 at the bottom
+                            ],
+                            stops: const [
+                              0.0,
+                              1.0
+                            ], // Gradual transition from top to bottom
+                          ).createShader(bounds);
+                        },
+                        blendMode: BlendMode.dstIn,
+                        child: ClipRRect(
+                            borderRadius: BorderRadius.circular(12),
+                            child: Image.asset(featureApp.icon)),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: ThemePaddings.normalPadding),
+                        child: Column(
+                          children: [
+                            Text(
+                              "Featured App",
+                              style: TextStyles.labelText,
+                            ),
+                            ThemedControls.spacerVerticalMini(),
+                            Text(
+                              featureApp.description,
+                              style: TextStyles.secondaryTextSmall,
+                              textAlign: TextAlign.center,
+                            )
+                          ],
                         ),
-                        Text(
-                          dAppsList[0].description,
-                          style: TextStyles.smallInfoText,
-                        ),
-                      ],
-                    ),
+                      )
+                    ],
                   ),
-                  ThemedControls.transparentButtonSmall(
-                      onPressed: () {}, text: "Open"),
-                ],
-              )
-            ],
-          ))
-        ],
-      ),
+                ),
+                Text(
+                  "Popular dApps",
+                  style: TextStyles.pageTitle,
+                ),
+                ThemedControls.card(
+                  child: Column(
+                    children: List.generate(dAppsList.length, (index) {
+                      return DAppTile(dApp: dAppsList[index]);
+                    }),
+                  ),
+                ),
+              ],
+            )
+          : const Center(
+              child:
+                  CircularProgressIndicator()), // Show loading until images are cached
     );
   }
 }
