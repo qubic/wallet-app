193a14a52c5ce4ee23eb3a56ca843bb45afc59b8
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-02-24 16:13:32 +0200

    feat: Add slide animation to TabDApps

--- lib/pages/main/tab_dapps/tab_dapps.dart
+++ lib/pages/main/tab_dapps/tab_dapps.dart
@@ -14,105 +14,211 @@ class TabDApps extends StatefulWidget {
   State<TabDApps> createState() => _TabDAppsState();
 }
 
-class _TabDAppsState extends State<TabDApps> {
+class _TabDAppsState extends State<TabDApps> with TickerProviderStateMixin {
   bool _isImagesLoaded = false;
+  bool _isFirst = true;
+  late AnimationController _featuredController;
+  late Animation<Offset> _featuredSlideAnimation;
+  late Animation<double> _fadeAnimation;
+  late AnimationController _popularController;
+  late Animation<Offset> _popularSlideAnimation;
+  late Animation<double> _popularFadeAnimation;
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
-    _preloadImages();
+    if (_isFirst) {
+      _initializeFeaturedAnimations();
+      _initializePopularAnimations();
+      _preloadImages();
+      _isFirst = false;
+    }
+  }
+
+  void _initializeFeaturedAnimations() {
+    _featuredController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    _featuredSlideAnimation = Tween<Offset>(
+      begin: const Offset(0, -1),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _featuredController,
+      curve: Curves.easeOutCubic,
+    ));
+
+    _fadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _featuredController,
+      curve: Curves.easeIn,
+    ));
+  }
+
+  void _initializePopularAnimations() {
+    _popularController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    );
+
+    _popularSlideAnimation = Tween<Offset>(
+      begin: const Offset(-1, 0),
+      end: Offset.zero,
+    ).animate(CurvedAnimation(
+      parent: _popularController,
+      curve: Curves.easeOutCubic,
+    ));
+
+    _popularFadeAnimation = Tween<double>(
+      begin: 0.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(
+      parent: _popularController,
+      curve: Curves.easeIn,
+    ));
   }
 
   Future<void> _preloadImages() async {
-    for (var dApp in dAppsList) {
-      await precacheImage(NetworkImage(dApp.icon), context);
-    }
+    await precacheImage(AssetImage(featureApp.icon), context);
+
     if (mounted) {
-      await precacheImage(AssetImage(featureApp.icon), context);
+      setState(() {
+        _isImagesLoaded = true;
+      });
+
+      _featuredController.forward();
+      _popularController.forward();
     }
-    setState(() {
-      _isImagesLoaded = true;
-    });
+  }
+
+  @override
+  void dispose() {
+    _featuredController.dispose();
+    _popularController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-      ),
+      appBar: AppBar(backgroundColor: Colors.transparent),
       body: _isImagesLoaded
           ? ListView(
               padding: ThemeEdgeInsets.pageInsets,
               children: [
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                WebviewScreen(initialUrl: featureApp.url)));
-                  },
-                  child: Stack(
-                    alignment: Alignment.center,
-                    children: [
-                      ShaderMask(
-                        shaderCallback: (Rect bounds) {
-                          return LinearGradient(
-                            begin: Alignment.topCenter,
-                            end: Alignment.bottomCenter,
-                            colors: [
-                              Colors.black.withValues(alpha: 0.7),
-                              Colors.transparent, // Opacity 0 at the bottom
-                            ],
-                            stops: const [
-                              0.0,
-                              1.0
-                            ], // Gradual transition from top to bottom
-                          ).createShader(bounds);
-                        },
-                        blendMode: BlendMode.dstIn,
-                        child: ClipRRect(
-                            borderRadius: BorderRadius.circular(12),
-                            child: Image.asset(featureApp.icon)),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: ThemePaddings.normalPadding),
-                        child: Column(
-                          children: [
-                            Text(
-                              "Featured App",
-                              style: TextStyles.labelText,
-                            ),
-                            ThemedControls.spacerVerticalMini(),
-                            Text(
-                              featureApp.description,
-                              style: TextStyles.secondaryTextSmall,
-                              textAlign: TextAlign.center,
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+                FeaturedAppWidget(
+                  slideAnimation: _featuredSlideAnimation,
+                  fadeAnimation: _fadeAnimation,
                 ),
-                Text(
-                  "Popular dApps",
-                  style: TextStyles.pageTitle,
-                ),
-                ThemedControls.card(
-                  child: Column(
-                    children: List.generate(dAppsList.length, (index) {
-                      return DAppTile(dApp: dAppsList[index]);
-                    }),
-                  ),
+                const SizedBox(height: 16),
+                const Text("Popular dApps", style: TextStyles.pageTitle),
+                PopularDAppsWidget(
+                  slideAnimation: _popularSlideAnimation,
+                  fadeAnimation: _popularFadeAnimation,
                 ),
               ],
             )
-          : const Center(
-              child:
-                  CircularProgressIndicator()), // Show loading until images are cached
+          : const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
+
+class FeaturedAppWidget extends StatelessWidget {
+  final Animation<Offset> slideAnimation;
+  final Animation<double> fadeAnimation;
+
+  const FeaturedAppWidget({
+    required this.slideAnimation,
+    required this.fadeAnimation,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => WebviewScreen(initialUrl: featureApp.url),
+          ),
+        );
+      },
+      child: SlideTransition(
+        position: slideAnimation,
+        child: FadeTransition(
+          opacity: fadeAnimation,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              ShaderMask(
+                shaderCallback: (Rect bounds) {
+                  return LinearGradient(
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    colors: [
+                      Colors.black.withOpacity(0.7),
+                      Colors.transparent,
+                    ],
+                    stops: const [0.0, 1.0],
+                  ).createShader(bounds);
+                },
+                blendMode: BlendMode.dstIn,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: Image.asset(featureApp.icon),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: ThemePaddings.normalPadding),
+                child: Column(
+                  children: [
+                    Text("Featured App", style: TextStyles.labelText),
+                    ThemedControls.spacerVerticalMini(),
+                    Text(
+                      featureApp.description,
+                      style: TextStyles.secondaryTextSmall,
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PopularDAppsWidget extends StatelessWidget {
+  final Animation<Offset> slideAnimation;
+  final Animation<double> fadeAnimation;
+
+  const PopularDAppsWidget({
+    required this.slideAnimation,
+    required this.fadeAnimation,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ThemedControls.card(
+      child: Column(
+        children: List.generate(dAppsList.length, (index) {
+          return SlideTransition(
+            position: slideAnimation,
+            child: FadeTransition(
+              opacity: fadeAnimation,
+              child: DAppTile(dApp: dAppsList[index]),
+            ),
+          );
+        }),
+      ),
     );
   }
 }
