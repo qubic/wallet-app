9da3dca24c9c32929e3923aaa482bc9a0d3fd7fd
sallymoc <sallymoc.qubic@gmail.com>
2025-02-26 15:09:46 +0100

    Smart contracts methods descriptions added

--- lib/smart_contracts/sc_info.dart
+++ lib/smart_contracts/sc_info.dart
@@ -21,9 +21,47 @@ enum QubicSCID {
   msVault(11, 'MSVault',
       'LAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAKPTJ');
 
-  //TODO Complete this map
   static final Map<String, Map<int, String>> procedureNames = {
-    qEarn.contractId: {1: 'Lock', 2: 'Unlock'},
+    qX.contractId: {
+      1: 'Qx Issue Asset',
+      2: 'Qx Transfer Shares',
+      5: 'Qx Add to Ask Orders',
+      6: 'Qx Add to Bid Orders',
+      7: 'Qx Remove from Ask Orders',
+      8: 'Qx Remove from Bid Orders'
+    },
+    quottery.contractId: {
+      1: 'Quottery Issue Bet',
+      2: 'Quottery Join Bet',
+      3: 'Quottery Cancel Bet',
+      4: 'Quottery Publish Result'
+    },
+    qRnd.contractId: {1: 'Random Reveal and Commit'},
+    qutil.contractId: {1: 'Send to Many', 2: 'Burn Qubic'},
+    gqmProp.contractId: {1: 'GQMPROP Create Proposal', 2: 'GQMPROP Vote'},
+    ccf.contractId: {1: 'CCF Create  Proposal', 2: 'CCF Vote'},
+    qEarn.contractId: {1: 'QEarn Lock', 2: 'QEarn Unlock'},
+    qVault.contractId: {
+      1: 'QVault Submit Auth Address',
+      2: 'QVault Change Auth Address',
+      3: 'QVault Submit Distribution Permille',
+      4: 'QVault Change Distribution Permille',
+      5: 'QVault Submit Reinvesting Address',
+      6: 'QVault Change Reinvesting Address',
+      7: 'QVault Submit Admin Address',
+      8: 'QVault Change Admin Address',
+      9: 'QVault Submit Banned Address',
+      10: 'QVault Save Banned Address',
+      11: 'QVault Submit Unbanned Address',
+      12: 'QVault Unblock Banned Address'
+    },
+    msVault.contractId: {
+      1: 'MSVault Register Vault',
+      2: 'MSVault Deposit',
+      3: 'MSVault Release',
+      4: 'MSVault Reset Release',
+      13: 'MSVault Vote Fee Change'
+    }
   };
 
   const QubicSCID(this.contractIndex, this.name, this.contractId);
