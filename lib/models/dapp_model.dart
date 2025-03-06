77859d0b887919a582001b2a104070770c741829
sallymoc <sallymoc.qubic@gmail.com>
2025-03-06 21:34:51 +0100

    Update list of dapps

--- lib/models/dapp_model.dart
+++ lib/models/dapp_model.dart
@@ -30,23 +30,37 @@ final List<DAppModel> dAppsList = [
   //   description: "Qubic network",
   // ),
   DAppModel(
+    name: "Qx",
+    icon:
+        "https://media.licdn.com/dms/image/v2/D560BAQFpyufUFSY-zg/company-logo_200_200/company-logo_200_200/0/1730965355499/qubicnetwork_logo?e=1749081600&v=beta&t=F9UXA_X3wNiQypJnc-kwjPvVw0EqeUr0q7oDibeukkk",
+    url: "https://dev.qx.qubic.org",
+    description: "1st decentralised exchange within the Qubic Ecosystem.",
+  ),
+  DAppModel(
+    name: "QEarn",
+    icon: "https://framerusercontent.com/images/OlRHyS54DdB86UYF7BlAZbFQ.png",
+    url: "https://www.qearn.org/",
+    description:
+        "A smart contract-based platform that allows users to lock Qubic for rewards.",
+  ),
+  DAppModel(
     name: "Uniswap",
     icon:
         "https://cdn.brandfetch.io/idoYtBNi2C/w/800/h/800/theme/light/symbol.png?c=1dxbfHSJFAPEGdCLU4o5B",
     url: "https://uniswap.org",
-    description: "Ethereum blockchain-based",
+    description: "Ethereum blockchain-based.",
   ),
   DAppModel(
     name: "OpenSea",
     icon:
         "https://pageflows.imgix.net/media/logos/opensea.png?auto=compress&ixlib=python-1.1.2&s=459d52d3fe14f0c5ab2ade3916d4c862",
     url: "https://opensea.io",
-    description: "NFT marketplace",
+    description: "NFT marketplace.",
   ),
   DAppModel(
     name: "1inch",
     icon: "https://1inch.io/img/pressRoom/1inch_without_text.webp",
     url: "https://1inch.io",
-    description: "DEX aggregator",
+    description: "DEX aggregator.",
   ),
 ];
