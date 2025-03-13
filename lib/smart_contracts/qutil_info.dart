b7e4da5ec353fdd3fddebf28067c8d8557a3afb6
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-03-13 22:07:55 +0200

    feat: Add send to many transfers to ExplorerResultPageTransactionItem

--- lib/smart_contracts/qutil_info.dart
+++ lib/smart_contracts/qutil_info.dart
@@ -1,4 +1,9 @@
 class QutilInfo {
   static const address =
       "EAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAVWRF";
+
+  static const sendToManyType = 1;
+
+  static bool isSendToManyTransfer(String? destId, int? inputType) =>
+      destId == address && inputType == sendToManyType;
 }
