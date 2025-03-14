4b60b076dc3b3f39e0650646d8212375bcd4b693
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-03-15 01:23:54 +0200

    feat: Add initial AddNetworkScreen UI

---
+++ lib/pages/main/wallet_contents/settings/networks/add_network_screen.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:qubic_wallet/flutter_flow/theme_paddings.dart';
+import 'package:qubic_wallet/l10n/l10n.dart';
+import 'package:qubic_wallet/styles/edge_insets.dart';
+import 'package:qubic_wallet/styles/input_decorations.dart';
+import 'package:qubic_wallet/styles/text_styles.dart';
+import 'package:qubic_wallet/styles/themed_controls.dart';
+
+class AddNetworkScreen extends StatefulWidget {
+  const AddNetworkScreen({super.key});
+
+  @override
+  State<AddNetworkScreen> createState() => _AddNetworkScreenState();
+}
+
+class _AddNetworkScreenState extends State<AddNetworkScreen> {
+  final addNetworkFormKey = GlobalKey<FormBuilderState>();
+  final TextEditingController networkNameController = TextEditingController();
+  final TextEditingController rpcUrlController = TextEditingController();
+  final TextEditingController liUrlController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    final l10n = l10nOf(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add Custom Network", style: TextStyles.textExtraLargeBold),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+      ),
+      body: SafeArea(
+        child: FormBuilder(
+          key: addNetworkFormKey,
+          child: Column(
+            children: [
+              Expanded(
+                child: ListView(
+                  padding: ThemeEdgeInsets.pageInsets,
+                  children: [
+                    Text(
+                      "Network Name",
+                      style: TextStyles.labelTextNormal,
+                    ),
+                    ThemedControls.spacerVerticalSmall(),
+                    FormBuilderTextField(
+                        name: "networkName",
+                        controller: networkNameController,
+                        decoration:
+                            ThemeInputDecorations.normalInputbox.copyWith(
+                          hintText: "Eg: Qubic Mainnet",
+                        )),
+                    ThemedControls.spacerVerticalNormal(),
+                    Row(
+                      children: [
+                        Text(
+                          "RPC URL",
+                          style: TextStyles.labelTextNormal,
+                        ),
+                        const Spacer(),
+                        ThemedControls.transparentButtonSmall(
+                            onPressed: () async {
+                              if (rpcUrlController.text.isNotEmpty == true) {
+                                rpcUrlController.clear();
+                              } else {
+                                final clipboardData = await Clipboard.getData(
+                                    Clipboard.kTextPlain);
+                                if (clipboardData != null) {
+                                  rpcUrlController.text = clipboardData.text!;
+                                }
+                              }
+                              setState(() {});
+                            },
+                            text: rpcUrlController.text.isNotEmpty == true
+                                ? l10n.generalButtonClear
+                                : l10n.generalButtonPaste),
+                      ],
+                    ),
+                    FormBuilderTextField(
+                        name: "rpcUrl",
+                        controller: rpcUrlController,
+                        decoration:
+                            ThemeInputDecorations.normalInputbox.copyWith(
+                          hintText: "Eg: https://rpc.qubic.org",
+                        )),
+                    ThemedControls.spacerVerticalNormal(),
+                    Row(
+                      children: [
+                        Text(
+                          "Qubic Li URL",
+                          style: TextStyles.labelTextNormal,
+                        ),
+                        const Spacer(),
+                        ThemedControls.transparentButtonSmall(
+                            onPressed: () async {
+                              if (liUrlController.text.isNotEmpty == true) {
+                                liUrlController.clear();
+                              } else {
+                                final clipboardData = await Clipboard.getData(
+                                    Clipboard.kTextPlain);
+                                if (clipboardData != null) {
+                                  liUrlController.text = clipboardData.text!;
+                                }
+                              }
+                              setState(() {});
+                            },
+                            text: liUrlController.text.isNotEmpty == true
+                                ? l10n.generalButtonClear
+                                : l10n.generalButtonPaste),
+                      ],
+                    ),
+                    FormBuilderTextField(
+                        name: "liUrl",
+                        controller: liUrlController,
+                        decoration:
+                            ThemeInputDecorations.normalInputbox.copyWith(
+                          hintText: "Eg: https://api.qubic.li",
+                        )),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Expanded(
+                      child: ThemedControls.transparentButtonBigWithChild(
+                          child: Padding(
+                              padding: const EdgeInsets.all(
+                                  ThemePaddings.smallPadding),
+                              child: Text(l10n.generalButtonCancel,
+                                  style: TextStyles.transparentButtonText)),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          })),
+                  ThemedControls.spacerHorizontalNormal(),
+                  Expanded(
+                      child: ThemedControls.primaryButtonBigWithChild(
+                          onPressed: () {},
+                          child: Padding(
+                              padding: const EdgeInsets.all(
+                                  ThemePaddings.smallPadding),
+                              child: Text(l10n.generalButtonSave,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyles.primaryButtonText))))
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
