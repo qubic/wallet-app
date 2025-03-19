f9f4c684a2e19766ab8c0eff438b2e08d491b83a
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-03-19 04:00:30 +0200

    fix: Make viewExplorerURL opens in a full screen

--- lib/helpers/explorer_helpers.dart
+++ lib/helpers/explorer_helpers.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:qubic_wallet/config.dart';
-import 'package:qubic_wallet/pages/main/tab_dapps/webview_screen.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 
 void viewAddressInExplorer(BuildContext context, String address) {
@@ -12,8 +11,7 @@ void viewTransactionInExplorer(BuildContext context, String trxId) {
 }
 
 void viewExplorerURL(BuildContext context, String pathToData) {
-  // TODO: display full screen and to get the base explorer URL from the new config
-  Navigator.push(context, MaterialPageRoute(builder: (context) {
-    return WebviewScreen(initialUrl: "${Config.URL_WebExplorer}/$pathToData");
-  }));
+  // TODO: Get the base explorer URL from the new config
+  launchUrlString("${Config.URL_WebExplorer}/$pathToData",
+      mode: LaunchMode.inAppWebView);
 }
