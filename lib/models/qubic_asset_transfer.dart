139d9fe27ee91320eefcac4a4b07f0adb3039643
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-04-05 10:26:06 +0200

    refactor: Rename assetIssuer to newOwnerAndPossessor

--- lib/models/qubic_asset_transfer.dart
+++ lib/models/qubic_asset_transfer.dart
@@ -1,20 +1,20 @@
 class QubicAssetTransfer {
   final String assetName;
-  final String assetIssuer;
+  final String newOwnerAndPossessor;
   final String numberOfUnits;
   QubicAssetTransfer(
       {required this.assetName,
-      required this.assetIssuer,
+      required this.newOwnerAndPossessor,
       required this.numberOfUnits});
 
   factory QubicAssetTransfer.fromJson(Map<String, dynamic> json) {
     return QubicAssetTransfer(
         assetName: json['assetName'],
-        assetIssuer: json['assetIssuer'],
+        newOwnerAndPossessor: json['assetIssuer'],
         numberOfUnits: json['numberOfUnits']['value']);
   }
 
   @override
   String toString() =>
-      'QubicAssetTransfer(assetName: $assetName, assetIssuer: $assetIssuer, numberOfUnits: $numberOfUnits)';
+      'QubicAssetTransfer(assetName: $assetName, newOwnerAndPossessor: $newOwnerAndPossessor, numberOfUnits: $numberOfUnits)';
 }
