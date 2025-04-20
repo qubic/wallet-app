95cc26d900f511f4b66eda28ab45962d60721789
sallymoc <sallymoc.qubic@gmail.com>
2025-04-20 20:22:52 +0200

    fix: removing support email as we will not support this option

--- lib/styles/app_icons.dart
+++ lib/styles/app_icons.dart
@@ -32,6 +32,5 @@ abstract class AppIcons {
   static const String iris = '${_path}iris.svg';
   static const String security = '${_path}security.svg';
   static const String support = '${_path}support.svg';
-  static const String email = '${_path}email.svg';
   static const String google = '${_path}google.svg';
 }
