7ad87791c2539e78145e1f848c81ffc12f3be7e2
sallymoc <sallymoc.qubic@gmail.com>
2025-04-24 02:58:15 +0200

    test: update to fail

--- lib/helpers/currency_helpers.dart
+++ lib/helpers/currency_helpers.dart
@@ -4,6 +4,6 @@ class CurrencyHelpers {
   static String formatToUsdCurrency(num amount) {
     final decimalDigits = (amount > 0 && amount < 1) ? 10 : 2;
     return NumberFormat.currency(symbol: '\$', decimalDigits: decimalDigits)
-        .format(amount);
+        .format(amount)
   }
 }
