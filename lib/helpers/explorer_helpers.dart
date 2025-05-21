5abc2962728c7cd2de3f1bfac945a2cfc4d10556
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-05-22 00:19:31 +0300

    fix: Add path

--- lib/helpers/explorer_helpers.dart
+++ lib/helpers/explorer_helpers.dart
@@ -16,7 +16,7 @@ void viewExplorerURL(BuildContext context, String pathToData) {
   final String explorerUrl = getIt<NetworkStore>().explorerUrl;
   pushScreen(
     context,
-    screen: WebviewScreen(initialUrl: explorerUrl),
+    screen: WebviewScreen(initialUrl: "$explorerUrl/$pathToData"),
     pageTransitionAnimation: PageTransitionAnimation.slideUp,
     withNavBar: false,
   );
