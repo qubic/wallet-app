4c07c564de09147a70f776d8ac2840ea00b333cb
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-06-27 15:59:32 +0300

    refactor: rename DappStore class

--- lib/stores/dapp_store.g.dart
+++ lib/stores/dapp_store.g.dart
@@ -8,38 +8,38 @@ part of 'dapp_store.dart';
 
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic, no_leading_underscores_for_local_identifiers
 
-mixin _$DappStore on _DappStore, Store {
+mixin _$DappStore on DappStoreBase, Store {
   Computed<List<DappDto>>? _$allDappsComputed;
 
   @override
   List<DappDto> get allDapps =>
       (_$allDappsComputed ??= Computed<List<DappDto>>(() => super.allDapps,
-              name: '_DappStore.allDapps'))
+              name: 'DappStoreBase.allDapps'))
           .value;
   Computed<List<DappDto>>? _$topDappsComputed;
 
   @override
   List<DappDto> get topDapps =>
       (_$topDappsComputed ??= Computed<List<DappDto>>(() => super.topDapps,
-              name: '_DappStore.topDapps'))
+              name: 'DappStoreBase.topDapps'))
           .value;
   Computed<DappDto?>? _$featuredDappComputed;
 
   @override
   DappDto? get featuredDapp =>
       (_$featuredDappComputed ??= Computed<DappDto?>(() => super.featuredDapp,
-              name: '_DappStore.featuredDapp'))
+              name: 'DappStoreBase.featuredDapp'))
           .value;
   Computed<List<DappDto>>? _$popularDappsComputed;
 
   @override
   List<DappDto> get popularDapps => (_$popularDappsComputed ??=
           Computed<List<DappDto>>(() => super.popularDapps,
-              name: '_DappStore.popularDapps'))
+              name: 'DappStoreBase.popularDapps'))
       .value;
 
   late final _$dappsResponseAtom =
-      Atom(name: '_DappStore.dappsResponse', context: context);
+      Atom(name: 'DappStoreBase.dappsResponse', context: context);
 
   @override
   DappsResponse? get dappsResponse {
@@ -54,7 +54,7 @@ mixin _$DappStore on _DappStore, Store {
     });
   }
 
-  late final _$errorAtom = Atom(name: '_DappStore.error', context: context);
+  late final _$errorAtom = Atom(name: 'DappStoreBase.error', context: context);
 
   @override
   String? get error {
@@ -70,7 +70,7 @@ mixin _$DappStore on _DappStore, Store {
   }
 
   late final _$isLoadingAtom =
-      Atom(name: '_DappStore.isLoading', context: context);
+      Atom(name: 'DappStoreBase.isLoading', context: context);
 
   @override
   bool get isLoading {
@@ -86,7 +86,7 @@ mixin _$DappStore on _DappStore, Store {
   }
 
   late final _$loadDappsAsyncAction =
-      AsyncAction('_DappStore.loadDapps', context: context);
+      AsyncAction('DappStoreBase.loadDapps', context: context);
 
   @override
   Future<void> loadDapps() {
