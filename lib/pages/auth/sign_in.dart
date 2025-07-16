d1aa67769e7433b34e0a21f7a59fdc6970db5290
Ahmed Tarek <ahmedtsalem9@gmail.com>
2025-07-16 12:58:54 +0300

    refactor(sign_in): change disableLocalAuth type from String to bool for improved clarity

--- lib/pages/auth/sign_in.dart
+++ lib/pages/auth/sign_in.dart
@@ -29,9 +29,8 @@ import 'package:universal_platform/universal_platform.dart';
 import 'package:qubic_wallet/l10n/l10n.dart';
 
 class SignIn extends StatefulWidget {
-  final String?
-      disableLocalAuth; //if not null then local auth is disabled. TODO: Change this to a boolean
-  const SignIn({super.key, this.disableLocalAuth});
+  final bool disableLocalAuth;
+  const SignIn({super.key, this.disableLocalAuth = false});
 
   @override
   // ignore: library_private_types_in_public_api
@@ -184,7 +183,7 @@ class _SignInState extends State<SignIn>
     if (firstRun) {
       //Automatic local authentication on launch of widget
       _disposeLocalAuth = autorun((_) {
-        if (((widget.disableLocalAuth == null)) &&
+        if ((!widget.disableLocalAuth) &&
             (_settingsStore.settings.biometricEnabled)) {
           setState(() {
             formOpacity = 0;
