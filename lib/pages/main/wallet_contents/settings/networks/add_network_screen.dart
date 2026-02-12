0307bff7ed5e3eda9c42bc3d1814e64bfd57f5f0
sallymoc <sallymoc.qubic@gmail.com>
2026-02-12 17:15:44 +0100

    feat: add ability to edit custom networks

--- lib/pages/main/wallet_contents/settings/networks/add_network_screen.dart
+++ lib/pages/main/wallet_contents/settings/networks/add_network_screen.dart
@@ -15,14 +15,16 @@ import 'package:qubic_wallet/styles/text_styles.dart';
 import 'package:qubic_wallet/styles/themed_controls.dart';
 
 class AddNetworkScreen extends StatefulWidget {
-  const AddNetworkScreen({super.key});
+  final NetworkModel? network;
+
+  const AddNetworkScreen({super.key, this.network});
 
   @override
   State<AddNetworkScreen> createState() => _AddNetworkScreenState();
 }
 
 class _AddNetworkScreenState extends State<AddNetworkScreen> {
-  final addNetworkFormKey = GlobalKey<FormBuilderState>();
+  final formKey = GlobalKey<FormBuilderState>();
   final TextEditingController networkNameController = TextEditingController();
   final TextEditingController rpcUrlController = TextEditingController();
   final TextEditingController explorerController = TextEditingController();
@@ -38,21 +40,38 @@ class _AddNetworkScreenState extends State<AddNetworkScreen> {
     ),
   );
 
+  bool get isEditing => widget.network != null;
+
+  @override
+  void initState() {
+    super.initState();
+    if (isEditing) {
+      networkNameController.text = widget.network!.name;
+      rpcUrlController.text = removeHttpsScheme(widget.network!.rpcUrl);
+      explorerController.text = removeHttpsScheme(widget.network!.explorerUrl);
+    }
+  }
+
   @override
   void dispose() {
     networkNameController.dispose();
     rpcUrlController.dispose();
+    explorerController.dispose();
     super.dispose();
   }
 
   onSubmitted() {
-    if (addNetworkFormKey.currentState!.validate()) {
+    if (formKey.currentState!.validate()) {
       final network = NetworkModel(
         name: networkNameController.text,
         rpcUrl: '$httpsScheme${rpcUrlController.text}',
         explorerUrl: '$httpsScheme${explorerController.text}',
       );
-      networkStore.addNetwork(network);
+      if (isEditing) {
+        networkStore.updateNetwork(widget.network!, network);
+      } else {
+        networkStore.addNetwork(network);
+      }
       Navigator.pop(context);
     }
   }
@@ -64,18 +83,30 @@ class _AddNetworkScreenState extends State<AddNetworkScreen> {
     return url;
   }
 
+  List<String> getNetworkNamesForValidation() {
+    if (isEditing) {
+      return networkStore.networks
+          .where((n) => n.name != widget.network!.name)
+          .map((e) => e.name)
+          .toList();
+    }
+    return networkStore.networks.map((e) => e.name).toList();
+  }
+
   @override
   Widget build(BuildContext context) {
     final l10n = l10nOf(context);
     return Scaffold(
       appBar: AppBar(
-        title: Text(l10n.addNetworkTitle, style: TextStyles.textExtraLargeBold),
+        title: Text(
+            isEditing ? l10n.editNetworkTitle : l10n.addNetworkTitle,
+            style: TextStyles.textExtraLargeBold),
         centerTitle: true,
         backgroundColor: Colors.transparent,
       ),
       body: SafeArea(
         child: FormBuilder(
-          key: addNetworkFormKey,
+          key: formKey,
           child: Column(
             children: [
               Expanded(
@@ -92,9 +123,7 @@ class _AddNetworkScreenState extends State<AddNetworkScreen> {
                         validator: FormBuilderValidators.compose([
                           FormBuilderValidators.required(),
                           CustomFormFieldValidators.isNameAvailable(
-                              namesList: networkStore.networks
-                                  .map((e) => e.name)
-                                  .toList(),
+                              namesList: getNetworkNamesForValidation(),
                               context: context)
                         ]),
                         decoration:
