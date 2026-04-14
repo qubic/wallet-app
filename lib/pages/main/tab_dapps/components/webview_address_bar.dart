1b9ca1c57ea4092a4edf1e4fce3cf9490063a7af
Ahmed Tarek <ahmed.tarek.abdelmoaty@nawy.com>
2026-04-14 16:35:09 +0200

    fix: anchor share popover to menu button

--- lib/pages/main/tab_dapps/components/webview_address_bar.dart
+++ lib/pages/main/tab_dapps/components/webview_address_bar.dart
@@ -135,10 +135,7 @@ class _WebviewAddressBarState extends State<WebviewAddressBar> {
 
   void _shareCurrentUrl(BuildContext context) async {
     if (widget.webViewController != null) {
-      final box = context.findRenderObject() as RenderBox?;
-      final sharePositionOrigin = box != null
-          ? box.localToGlobal(Offset.zero) & box.size
-          : const Rect.fromLTWH(0, 0, 1, 1);
+      final sharePositionOrigin = _getMenuButtonRect();
       final url = await widget.webViewController!.getUrl();
       if (url != null && mounted) {
         await Share.share(
